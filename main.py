732b30e66bf5612253eafae5810bba100c419cb6
GaoGaoTaiga <nmmarzano@users.noreply.github.com>
2018-06-23 17:21:04 -0300

    Finish logic for finding recipes, getting user input

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 
-INGREDIENT = {"TinyMushroom":0,"BlukBerry":1,"Apricorn":2,"Fossil":3,"BigRoot":4,"IcyRock":5,"Honey":6,"BalmMushroom":7,"RainbowMatter":8,"MysticalShell":9}
-QUALITY = {"Normal":0,"Good":1,"VeryGood":2,"Special":3}
+INGREDIENT = {"Tiny Mushroom":0,"Bluk Berry":1,"Apricorn":2,"Fossil":3,"Big Root":4,"Icy Rock":5,"Honey":6,"Balm Mushroom":7,"Rainbow Matter":8,"Mystical Shell":9}
+INVERSE_INGREDIENT = {INGREDIENT[k]:k for k in INGREDIENT}
+QUALITY = {"Normal":0,"Good":1,"Very Good":2,"Special":3}
+INVERSE_QUALITY = {QUALITY[k]:k for k in QUALITY}
+POT = {"Normal":3,"Bronze":10,"Silver":15,"Gold":20}
 RECIPES = {
-	"MulliganStew":{
+	"Mulligan Stew":{
 		QUALITY["Special"]:{
-			INGREDIENT["RainbowMatter"]:5
+			INGREDIENT["Rainbow Matter"]:5
 		}
 	},
-	"RedStew":{
+	"Red Stew":{
 		QUALITY["Normal"]:{
-			INGREDIENT["TinyMushroom"]:5
+			INGREDIENT["Tiny Mushroom"]:5
 		},
 		QUALITY["Special"]:{
-			INGREDIENT["BigRoot"]:5
+			INGREDIENT["Big Root"]:5
 		}
 	},
-	"BlueSoda":{
+	"Blue Soda":{
 		QUALITY["Normal"]:{
-			INGREDIENT["BlukBerry"]:5
+			INGREDIENT["Bluk Berry"]:5
 		},
 		QUALITY["Special"]:{
-			INGREDIENT["IcyRock"]:5
+			INGREDIENT["Icy Rock"]:5
 		}
 	},
-	"YellowCurry":{
+	"Yellow Curry":{
 		QUALITY["Normal"]:{
 			INGREDIENT["Apricorn"]:5
 		},
@@ -31,31 +34,115 @@ RECIPES = {
 			INGREDIENT["Honey"]:5
 		}
 	},
-	"GreyPorridge":{
+	"Grey Porridge":{
 		QUALITY["Normal"]:{
 			INGREDIENT["Fossil"]:5
 		},
 		QUALITY["Special"]:{
-			INGREDIENT["BalmMushroom"]:5
+			INGREDIENT["Balm Mushroom"]:5
 		}
 	},
+	"Mouth-Watering Dip":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Bluk Berry"]:3,
+			INGREDIENT["Tiny Mushroom"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Bluk Berry"]:3,
+			INGREDIENT["Apricorn"]:1,
+			INGREDIENT["Honey"]:1
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Icy Rock"]:1,
+			INGREDIENT["Bluk Berry"]:2,
+			INGREDIENT["Honey"]:2
+		}
+	},
+	"Plain Crepe":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Bluk Berry"]:3,
+			INGREDIENT["Fossil"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Bluk Berry"]:2,
+			INGREDIENT["Honey"]:1,
+			INGREDIENT["Fossil"]:2
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Bluk Berry"]:1,
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Balm Mushroom"]:2
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Honey"]:3,
+			INGREDIENT["Balm Mushroom"]:2
+		}
+	},
+	"Sludge Soup":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Tiny Mushroom"]:3,
+			INGREDIENT["Balm Mushroom"]:2
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Tiny Mushroom"]:2,
+			INGREDIENT["Balm Mushroom"]:3
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Tiny Mushroom"]:1,
+			INGREDIENT["Balm Mushroom"]:3,
+			INGREDIENT["Rainbow Matter"]:1
+		}
+	}
 }
 
-def printRecipes():
-	for recipe in RECIPES:
-		print(recipe+":")
-		for quality in RECIPES[recipe]:
-			for reverseQuality in QUALITY:
-				if(quality==QUALITY[reverseQuality]):
-					print("\t"+reverseQuality+":")
-			for ingredient in RECIPES[recipe][quality]:
-				for reverseIngredient in INGREDIENT:
-					if(ingredient==INGREDIENT[reverseIngredient]):
-						print("\t\t"+reverseIngredient+":",RECIPES[recipe][quality][ingredient])
+def printRecipes(recipes):
+	for recipe in recipes:
+		print(recipe+" à la Cube:")
+		for quality in recipes[recipe]:
+			print("\t"+INVERSE_QUALITY[quality]+":")
+			for ingredient in recipes[recipe][quality]:
+				print("\t\t"+INVERSE_INGREDIENT[ingredient]+":",recipes[recipe][quality][ingredient])
 		print()
 
+def getPossibleRecipes(userIngredients, quantity):
+	possibleRecipes = {}
+	canMake = 1
+	for recipe in RECIPES:
+		for quality in RECIPES[recipe]:
+			canMake = 1
+			for ingredient in RECIPES[recipe][quality]:
+				if userIngredients[INVERSE_INGREDIENT[ingredient]] < RECIPES[recipe][quality][ingredient]*quantity:
+					canMake = 0
+			if canMake == 1:
+				possibleRecipes[recipe] = {}
+				possibleRecipes[recipe][quality] = RECIPES[recipe][quality]
+	return possibleRecipes
+
+def getIngredientQuantities():
+	userIngredients = {}
+	print("Enter ingredient quantities:")
+	userIngredients["Tiny Mushroom"] = int(input("Tiny Mushrooms: "))
+	userIngredients["Bluk Berry"] = int(input("Bluk Berries: "))
+	userIngredients["Apricorn"] = int(input("Apricorns: "))
+	userIngredients["Fossil"] = int(input("Fossils: "))
+	userIngredients["Big Root"] = int(input("Big Roots: "))
+	userIngredients["Icy Rock"] = int(input("Icy Rocks: "))
+	userIngredients["Honey"] = int(input("Honey: "))
+	userIngredients["Balm Mushroom"] = int(input("Balm Mushrooms: "))
+	userIngredients["Rainbow Matter"] = int(input("Rainbow Matter: "))
+	userIngredients["Mystical Shell"] = int(input("Mystical Shells: "))
+	return userIngredients
+
 def main():
-	printRecipes()
+	userIngredients = getIngredientQuantities()
+	print("\nNormal Pot:")
+	printRecipes(getPossibleRecipes(userIngredients, POT["Normal"]))
+	print("\nBronze Pot:")
+	printRecipes(getPossibleRecipes(userIngredients, POT["Bronze"]))
+	print("\nSilver Pot:")
+	printRecipes(getPossibleRecipes(userIngredients, POT["Silver"]))
+	print("\nGold Pot:")
+	printRecipes(getPossibleRecipes(userIngredients, POT["Gold"]))
 
 if __name__ == "__main__":
 	main()
