b26cda767e66e940ac042ac3e1e42a5b0c05db5c
GaoGaoTaiga <nmmarzano@users.noreply.github.com>
2018-06-23 21:10:37 -0300

    Finish adding recipes, take out mulligan stew and mystical shell

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 
-INGREDIENT = {"Tiny Mushroom":0,"Bluk Berry":1,"Apricorn":2,"Fossil":3,"Big Root":4,"Icy Rock":5,"Honey":6,"Balm Mushroom":7,"Rainbow Matter":8,"Mystical Shell":9}
+INGREDIENT = {"Tiny Mushroom":0,"Bluk Berry":1,"Apricorn":2,"Fossil":3,"Big Root":4,"Icy Rock":5,"Honey":6,"Balm Mushroom":7,"Rainbow Matter":8}
 INVERSE_INGREDIENT = {INGREDIENT[k]:k for k in INGREDIENT}
 QUALITY = {"Normal":0,"Good":1,"Very Good":2,"Special":3}
 INVERSE_QUALITY = {QUALITY[k]:k for k in QUALITY}
 POT = {"Normal":3,"Bronze":10,"Silver":15,"Gold":20}
 RECIPES = {
-	"Mulligan Stew":{
-		QUALITY["Special"]:{
-			INGREDIENT["Rainbow Matter"]:5
-		}
-	},
 	"Red Stew":{
 		QUALITY["Normal"]:{
 			INGREDIENT["Tiny Mushroom"]:5
@@ -92,16 +87,177 @@ RECIPES = {
 			INGREDIENT["Balm Mushroom"]:3,
 			INGREDIENT["Rainbow Matter"]:1
 		}
+	},
+	"Mud Pie":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Tiny Mushroom"]:3,
+			INGREDIENT["Fossil"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Tiny Mushroom"]:3,
+			INGREDIENT["Icy Rock"]:2
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Icy Rock"]:2,
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Bluk Berry"]:1
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Honey"]:3,
+			INGREDIENT["Icy Rock"]:2
+		}
+	},
+	"Veggie Smoothie":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Big Root"]:3,
+			INGREDIENT["Balm Mushroom"]:1,
+			INGREDIENT["Apricorn"]:1
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Apricorn"]:3,
+			INGREDIENT["Tiny Mushroom"]:1,
+			INGREDIENT["Big Root"]:1
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Big Root"]:3,
+			INGREDIENT["Apricorn"]:2
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Big Root"]:3,
+			INGREDIENT["Apricorn"]:1,
+			INGREDIENT["Rainbow Matter"]:1
+		}
+	},
+	"Honey Nectar":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Bluk Berry"]:2,
+			INGREDIENT["Apricorn"]:1
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Honey"]:3,
+			INGREDIENT["Bluk Berry"]:2
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Honey"]:3,
+			INGREDIENT["Icy Rock"]:1,
+			INGREDIENT["Bluk Berry"]:1
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Honey"]:3,
+			INGREDIENT["Bluk Berry"]:1,
+			INGREDIENT["Rainbow Matter"]:1
+		}
+	},
+	"Brain Food":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Bluk Berry"]:3,
+			INGREDIENT["Apricorn"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Bluk Berry"]:2,
+			INGREDIENT["Apricorn"]:2,
+			INGREDIENT["Honey"]:1
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Icy Rock"]:1,
+			INGREDIENT["Apricorn"]:1,
+			INGREDIENT["Bluk Berry"]:1
+		}
+	},
+	"Stone Soup":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Fossil"]:3,
+			INGREDIENT["Apricorn"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Apricorn"]:2,
+			INGREDIENT["Fossil"]:2,
+			INGREDIENT["Icy Rock"]:1
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Fossil"]:1,
+			INGREDIENT["Icy Rock"]:3,
+			INGREDIENT["Apricorn"]:1
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Icy Rock"]:3,
+			INGREDIENT["Fossil"]:1,
+			INGREDIENT["Rainbow Matter"]:1
+		}
+	},
+	"Light-as-Air Casserole":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Fossil"]:2,
+			INGREDIENT["Big Root"]:2,
+			INGREDIENT["Icy Rock"]:1
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Icy Rock"]:3,
+			INGREDIENT["Big Root"]:2
+		}
+	},
+	"Hot Pot":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Tiny Mushroom"]:3,
+			INGREDIENT["Bluk Berry"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Tiny Mushroom"]:3
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Balm Mushroom"]:3,
+			INGREDIENT["Big Root"]:2
+		}
+	},
+	"Watt a Risotto":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Tiny Mushroom"]:2,
+			INGREDIENT["Apricorn"]:1
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Balm Mushroom"]:1,
+			INGREDIENT["Big Root"]:1,
+			INGREDIENT["Apricorn"]:1
+		},
+		QUALITY["Special"]:{
+			INGREDIENT["Honey"]:3,
+			INGREDIENT["Balm Mushroom"]:1,
+			INGREDIENT["Big Root"]:1
+		}
+	},
+	"Get Swole Syrup":{
+		QUALITY["Normal"]:{
+			INGREDIENT["Honey"]:1,
+			INGREDIENT["Bluk Berry"]:2,
+			INGREDIENT["Tiny Mushroom"]:2
+		},
+		QUALITY["Good"]:{
+			INGREDIENT["Bluk Berry"]:2,
+			INGREDIENT["Tiny Mushroom"]:1,
+			INGREDIENT["Honey"]:1,
+			INGREDIENT["Balm Mushroom"]:1
+		},
+		QUALITY["Very Good"]:{
+			INGREDIENT["Bluk Berry"]:1,
+			INGREDIENT["Tiny Mushroom"]:1,
+			INGREDIENT["Honey"]:2,
+			INGREDIENT["Balm Mushroom"]:1
+		}
 	}
 }
 
 def printRecipes(recipes):
 	for recipe in recipes:
-		print(recipe+" à la Cube:")
+		print("\t"+recipe+" à la Cube:")
 		for quality in recipes[recipe]:
-			print("\t"+INVERSE_QUALITY[quality]+":")
+			print("\t\t"+INVERSE_QUALITY[quality]+":")
 			for ingredient in recipes[recipe][quality]:
-				print("\t\t"+INVERSE_INGREDIENT[ingredient]+":",recipes[recipe][quality][ingredient])
+				print("\t\t\t"+INVERSE_INGREDIENT[ingredient]+":",recipes[recipe][quality][ingredient])
 		print()
 
 def getPossibleRecipes(userIngredients, quantity):
@@ -114,8 +270,11 @@ def getPossibleRecipes(userIngredients, quantity):
 				if userIngredients[INVERSE_INGREDIENT[ingredient]] < RECIPES[recipe][quality][ingredient]*quantity:
 					canMake = 0
 			if canMake == 1:
-				possibleRecipes[recipe] = {}
-				possibleRecipes[recipe][quality] = RECIPES[recipe][quality]
+				try:
+					possibleRecipes[recipe][quality] = RECIPES[recipe][quality]
+				except KeyError:
+					possibleRecipes[recipe] = {}
+					possibleRecipes[recipe][quality] = RECIPES[recipe][quality]
 	return possibleRecipes
 
 def getIngredientQuantities():
@@ -130,7 +289,6 @@ def getIngredientQuantities():
 	userIngredients["Honey"] = int(input("Honey: "))
 	userIngredients["Balm Mushroom"] = int(input("Balm Mushrooms: "))
 	userIngredients["Rainbow Matter"] = int(input("Rainbow Matter: "))
-	userIngredients["Mystical Shell"] = int(input("Mystical Shells: "))
 	return userIngredients
 
 def main():
